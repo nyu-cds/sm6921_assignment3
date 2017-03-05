793700413b4e11cfb9bd37e8fdfcf1dd28c00803
Saurabh Mahajan <saurabh.nmahajan@gmail.com>
2017-03-05 15:52:54 -0500

    Decreased the runtime (Assignment 5)

---
+++ calculator.py
@@ -0,0 +1,15 @@
+# -----------------------------------------------------------------------------
+# calculator.py
+# ----------------------------------------------------------------------------- 
+# relative speedup = 2.45/0.038 = 64.47
+import numpy as np
+
+def hypotenuse(x,y):
+    """
+    Return sqrt(x**2 + y**2) for two arrays, a and b.
+    x and y must be two-dimensional arrays of the same shape.
+    """
+    xx = np.multiply(x,x)
+    yy = np.multiply(y,y)
+    zz = np.add(xx, yy)
+    return np.sqrt(zz)
