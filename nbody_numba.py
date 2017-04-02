c8681ef401e2ce2a4d86146d153abcb652b543fb
Saurabh Mahajan <saurabh.nmahajan@gmail.com>
2017-04-02 12:54:05 -0400

    Added vectorized function

--- nbody_numba.py
+++ nbody_numba.py
@@ -1,9 +1,15 @@
 """
     Added jit decorators and used numba function signatures.
+    Added vectorized function vec_deltas to compute deltas in advance and report_energy
 """
 
 from itertools import combinations
-from numba import jit, int64, float32, float64
+from numba import jit, int64, float32, float64, vectorize
+import numpy as np
+@vectorize([float64(float64, float64)])
+def vec_deltas(x, y):
+    return x - y
+
 @jit('void(float64[:,:,:], char[:,:], int32, float64, float64[:,:,:])')
 def advance(BODIES, combination, iterations, dt, values):
     for _ in range(iterations):
@@ -11,11 +17,9 @@ def advance(BODIES, combination, iterations, dt, values):
             advance by one time step
         '''
         for body1, body2 in combination:
-            ([x1, y1, z1], v1, m1) = BODIES[body1]
-            ([x2, y2, z2], v2, m2) = BODIES[body2]
-            dx = x1-x2
-            dy = y1-y2
-            dz = z1-z2
+            (u1, v1, m1) = BODIES[body1]
+            (u2, v2, m2) = BODIES[body2]
+            (dx, dy , dz) = vec_deltas(u1, u2)
             tmp = dt * ((dx * dx + dy * dy + dz * dz) ** (-1.5))
             m2_tmp = m2 * tmp
             m1_tmp = m1 * tmp
@@ -38,11 +42,9 @@ def report_energy(BODIES, combination, values, e=0.0):
         compute the energy and return it so that it can be printed
     '''
     for body1, body2 in combination:
-        ((x1, y1, z1), v1, m1) = BODIES[body1]
-        ((x2, y2, z2), v2, m2) = BODIES[body2]
-        dx = x1-x2
-        dy = y1-y2
-        dz = z1-z2
+        (u1, v1, m1) = BODIES[body1]
+        (u2, v2, m2) = BODIES[body2]
+        (dx, dy , dz) = vec_deltas(u1, u2)
         e -= (m1 * m2) / ((dx * dx + dy * dy + dz * dz) ** 0.5)
         
     for body in values:
@@ -82,38 +84,38 @@ def nbody(loops, reference, iterations):
     DAYS_PER_YEAR = 365.24
 
     BODIES = {
-        'sun': ([0.0, 0.0, 0.0], [0.0, 0.0, 0.0], SOLAR_MASS),
+        'sun': (np.array([0.0, 0.0, 0.0]), np.array([0.0, 0.0, 0.0]), SOLAR_MASS),
 
-        'jupiter': ([4.84143144246472090e+00,
+        'jupiter': (np.array([4.84143144246472090e+00,
                      -1.16032004402742839e+00,
-                     -1.03622044471123109e-01],
-                    [1.66007664274403694e-03 * DAYS_PER_YEAR,
+                     -1.03622044471123109e-01]),
+                    np.array([1.66007664274403694e-03 * DAYS_PER_YEAR,
                      7.69901118419740425e-03 * DAYS_PER_YEAR,
-                     -6.90460016972063023e-05 * DAYS_PER_YEAR],
+                     -6.90460016972063023e-05 * DAYS_PER_YEAR]),
                     9.54791938424326609e-04 * SOLAR_MASS),
 
-        'saturn': ([8.34336671824457987e+00,
+        'saturn': (np.array([8.34336671824457987e+00,
                     4.12479856412430479e+00,
-                    -4.03523417114321381e-01],
-                   [-2.76742510726862411e-03 * DAYS_PER_YEAR,
+                    -4.03523417114321381e-01]),
+                   np.array([-2.76742510726862411e-03 * DAYS_PER_YEAR,
                     4.99852801234917238e-03 * DAYS_PER_YEAR,
-                    2.30417297573763929e-05 * DAYS_PER_YEAR],
+                    2.30417297573763929e-05 * DAYS_PER_YEAR]),
                    2.85885980666130812e-04 * SOLAR_MASS),
 
-        'uranus': ([1.28943695621391310e+01,
+        'uranus': (np.array([1.28943695621391310e+01,
                     -1.51111514016986312e+01,
-                    -2.23307578892655734e-01],
-                   [2.96460137564761618e-03 * DAYS_PER_YEAR,
+                    -2.23307578892655734e-01]),
+                   np.array([2.96460137564761618e-03 * DAYS_PER_YEAR,
                     2.37847173959480950e-03 * DAYS_PER_YEAR,
-                    -2.96589568540237556e-05 * DAYS_PER_YEAR],
+                    -2.96589568540237556e-05 * DAYS_PER_YEAR]),
                    4.36624404335156298e-05 * SOLAR_MASS),
 
-        'neptune': ([1.53796971148509165e+01,
+        'neptune': (np.array([1.53796971148509165e+01,
                      -2.59193146099879641e+01,
-                     1.79258772950371181e-01],
-                    [2.68067772490389322e-03 * DAYS_PER_YEAR,
+                     1.79258772950371181e-01]),
+                    np.array([2.68067772490389322e-03 * DAYS_PER_YEAR,
                      1.62824170038242295e-03 * DAYS_PER_YEAR,
-                     -9.51592254519715870e-05 * DAYS_PER_YEAR],
+                     -9.51592254519715870e-05 * DAYS_PER_YEAR]),
                     5.15138902046611451e-05 * SOLAR_MASS)}
 
     # Set up global state
